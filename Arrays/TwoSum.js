7405a2d129c350003fe9df2ea52f6a1f6fa118f8
Kunal Kamthe <kunalkamthe200@gmail.com>
2025-02-06 23:07:43 +0530

    used the proper folder structure for the code

--- Arrays/TwoSum.js
+++ Arrays/TwoSum.js
@@ -4,7 +4,6 @@ function twoSumProblem(a, t) {
   for (let i = 0; i < n; i++) {
     for (let j = i + 1; j < n; j++) {
       if (a[i] + a[j] == t) {
-        // console.log("inside loop", i, j);
         return true;
       }
     }
@@ -17,5 +16,3 @@ const arr =[1, -2, 1, 0, 5]
 const target = 0;
 
 console.log(twoSumProblem(arr, target))
-
-//output - true- There is a pair (1, -3) with the sum equal to given target, 1 + (-3) = -2.
